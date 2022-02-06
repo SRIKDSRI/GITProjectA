b81e2144e66aec416f3baed730c0e900cc74fa66
srikanth <d.srikanth.rover@gmail.com>
2022-02-06 17:51:34 +0530

    upload to dev

--- src/com/qa/apack/Parent.java
+++ src/com/qa/apack/Parent.java
@@ -3,7 +3,7 @@ package com.qa.apack;
 public class Parent {
 	
 	public void test() {
-		System.out.println("Now I made the change");
+		System.out.println("Now user B has made the change");
 	}
 
 }
