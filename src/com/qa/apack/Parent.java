3381d6507909b0d5aa23fd76ddaa324d0152d092
srikanth <d.srikanth.rover@gmail.com>
2022-02-06 17:40:40 +0530

    change made

--- src/com/qa/apack/Parent.java
+++ src/com/qa/apack/Parent.java
@@ -3,7 +3,7 @@ package com.qa.apack;
 public class Parent {
 	
 	public void test() {
-		System.out.println("Am yet to make the modification");
+		System.out.println("Now I made the change");
 	}
 
 }
