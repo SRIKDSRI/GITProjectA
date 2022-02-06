f6c987308569d53270c39068c691131c3a2c8697
srikanth <d.srikanth.rover@gmail.com>
2022-02-06 22:47:03 +0530

    just now

--- src/com/qa/apack/Parent.java
+++ src/com/qa/apack/Parent.java
@@ -3,7 +3,7 @@ package com.qa.apack;
 public class Parent {
 	
 	public void test() {
-		System.out.println("Now user B1 has made the change");
+		System.out.println("Now user B5 has made the change");
 	}
 
 }
